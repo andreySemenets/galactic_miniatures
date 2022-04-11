4689b053f319318fabb922da4cea4d5795bd1453
Andrey Semenets <andreysemenets@MacBook-Air-Dara.local>
2022-04-11 22:18:36 +0300

    back color

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import './App.css';
 import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import Registration from './components/Registration/Registration';
 import Login from './components/Login/Login';
 import { checkUserAuth } from './redux/actions/userAC';
+import ModelPage from './components/ModelPage/ModelPage';
 
 const App = () => {
 	const dispatch = useDispatch();
@@ -23,6 +23,8 @@ const App = () => {
 			<Routes>
 				<Route path="/auth/login" element={<Login />} />
 				<Route path="/auth/registration" element={<Registration />} />
+				{/* TODO: ПОТОМ ПЕРЕДЕЛАТЬ НА ПАРАМС ЗАПРОС!!! */}
+				<Route path="/models" element={<ModelPage />} />
 			</Routes>
 		</>
 	);
