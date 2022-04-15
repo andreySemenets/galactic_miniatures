f306cba3d74be3436e3011859142384bba925fcb
Andrey Semenets <andreysemenets@MacBook-Air-Dara.local>
2022-04-15 13:18:26 +0300

    fileUploader settings in app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 const dbCheck = require('./helpers/dbCheck');
 const authRouter = require('./routers/authRouter');
 const errorMiddleware = require('./middlewares/errorMiddleware');
@@ -12,10 +13,11 @@ const { PORT } = process.env;
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(fileUpload({}));
 app.use(cookieParser());
 app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000'],
+	credentials: true,
+	origin: ['http://localhost:3000'],
 }));
 
 // routers
@@ -24,13 +26,13 @@ app.use('/auth', authRouter);
 app.use(errorMiddleware);
 
 const start = async () => {
-  try {
-    app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
-    dbCheck();
-  } catch (error) {
-    console.log(error);
-    console.log('Something went wrong with server connection to the PORT');
-  }
+	try {
+		app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+		dbCheck();
+	} catch (error) {
+		console.log(error);
+		console.log('Something went wrong with server connection to the PORT');
+	}
 };
 
 start();
