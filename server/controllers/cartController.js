fd25aaac656a888dada391309b13c296da621413
Sergey <sincer89@mail.ru>
2022-04-20 17:25:45 +0500

    quantity PLUS and Minus

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -36,7 +36,6 @@ module.exports.getUsersCartItems = async (req, res, next) => {
 
 module.exports.deleteItemCart = async (req, res, next) => {
 	try {
-		console.log('deleteItemCart' ,req.params)
 		const {userId,itemId } = req.params
 		const itemDelete = await ShoppingCart.findOne({where: {id: itemId,}});
 		itemDelete.destroy()
@@ -56,5 +55,73 @@ module.exports.deleteItemCart = async (req, res, next) => {
 
 	}  catch (error) {
 		console.log(error, 'deleteItemCart ERROR')
+		next()
+	}
+}
+
+module.exports.plusItemCart = async (req,res, next) => {
+	try {
+		const {userId,itemId } = req.params
+		const itemPlus = await ShoppingCart.findOne({where: {id: itemId,}});
+		itemPlus.set({quantity: itemPlus.quantity + 1})
+		await itemPlus.save()
+		const result = await ShoppingCart.findAll({
+			raw: true,
+			where: {
+				userId: +userId,
+			},
+			include: [
+				{
+					model: PhysicalCopy,
+					attributes: ['itemId', 'color', 'scale', 'price'],
+				},
+			],
+		});
+		res.json(result);
+	}  catch (error) {
+		console.log(error, 'plusItemCart ERROR')
+		next()
+	}
+}
+
+module.exports.minusItemCart = async (req,res, next) => {
+	try {
+		const {userId,itemId } = req.params
+		const itemPlus = await ShoppingCart.findOne({where: {id: itemId,}});
+		if (itemPlus.quantity === 1) {
+			const result = await ShoppingCart.findAll({
+				raw: true,
+				where: {
+					userId: +userId,
+				},
+				include: [
+					{
+						model: PhysicalCopy,
+						attributes: ['itemId', 'color', 'scale', 'price'],
+					},
+				],
+			});
+			res.json(result);
+		} else {
+			itemPlus.set({quantity: itemPlus.quantity - 1})
+			await itemPlus.save()
+			const result = await ShoppingCart.findAll({
+				raw: true,
+				where: {
+					userId: +userId,
+				},
+				include: [
+					{
+						model: PhysicalCopy,
+						attributes: ['itemId', 'color', 'scale', 'price'],
+					},
+				],
+			});
+			res.json(result);
+		}
+
+	}  catch (error) {
+		console.log(error, 'minusItemCart ERROR')
+		next()
 	}
 }
